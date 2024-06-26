0942f93cdcc344857f9f516f93bf9b30fa83512e
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-06-26 14:41:32 +0300

    alx

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -7,7 +7,7 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ 
+    """
     BasicCache: class that inherits from BaseCaching and is a caching system
     """
     def put(self, key, item):
